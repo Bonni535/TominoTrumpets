1b36b1a5ad0dc5e63b6173e72932feb6e78de875
Luca Bonini <luca14bonini@gmail.com>
2024-03-04 19:28:21 -0800

    Models and Classes created

---
+++ Models/Genre.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TominoTrumpets.Models;
+
+
+public class Genre
+{
+    public int Id { get; set; }
+    public string Description { get; set; }
+
+}
---
+++ Models/Song_Genre.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TominoTrumpets.Models;
+
+public class Song_Genre
+{
+    public int Id { get; set; }
+    public int SongId { get; set; }
+    public int GenreId { get; set; }
+}
