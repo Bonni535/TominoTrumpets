8e8dc353914231aff093f09a00d92f35d1d0069d
Luca Bonini <luca14bonini@gmail.com>
2024-03-05 18:11:14 -0800

    Fixed some bugs

--- Models/Artist.cs
+++ Models/Artist.cs
@@ -6,7 +6,7 @@ namespace TominoTrumpets.Models;
 public class Artist
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public int Age { get; set; }
     public string Bio { get; set; }
 }
